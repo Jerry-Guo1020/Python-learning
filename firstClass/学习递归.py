4cec153825885f6106b9eba04b130b60ba4d1216
Jerry-Guo1020 <2593517388@qq.com>
2025-08-26 23:30:59 +0800

    学习了Python基础递归函数知识内容

--- firstClass/学习递归.py
+++ firstClass/学习递归.py
@@ -10,25 +10,3 @@ print(factorial(number))
 print("%d 的阶层是：%d" % (number, result))
 
 
-# 课间练习:假设我们需要求出经历了20个月后，总共有多少对小兔崽子?(选代 vs 递归)
-def fab(n):
-    n1 = 1
-    n2 = 1
-    n3 = 1
-
-    if n < 1:
-        print('输入有误！')
-        return -1
-
-
-    while (n - 2) > 0:
-        n3 = n2 + n1
-        n1 = n2
-        n2 = n3
-        n -= 1
-
-    return n3
-
-result = fab(20)
-if result != -1:
-    print('总共有%d对小兔崽子诞生' % result)
